8149308f50fa4336a7f70086a654eec586131ba9
trmaa <trmaayt@gmail.com>
2023-12-17 21:34:22 +0100

    some concepts

--- render.js
+++ render.js
@@ -1,15 +1,18 @@
 class Pixel{
   color = new color(0,0,0);
-  position = new vec2(0,0);
-  constructor(color,position){
+  position2 = new vec2(0,0);
+  position3 = new vec3(0,0,0);
+  constructor(color,position2){
     this.color = color;
-    this.position = position;
+    this.position2 = position2;
   }
 }
 
 class Renderer{
   static cvs = document.querySelectorAll("canvas")[0];
   static ctx = Renderer.cvs.getContext("2d");
+
+  static fov = 25;
 
   static pixels = [];
 
@@ -22,6 +25,10 @@ class Renderer{
         let id = y*Renderer.cvs.width-Renderer.cvs.width+x;
         let maxid = Renderer.cvs.height*Renderer.cvs.width;
         Renderer.pixels[id] = new Pixel(new color(0,0,0),new vec2(x,y));
+
+        let w = x-Math.floor(Renderer.cvs.width*0.5);
+        let j = y-Math.floor(Renderer.cvs.height*0.5);
+        Renderer.pixels[id].position3 = new vec3(w,j,Renderer.fov);
       }
     }
   }
@@ -30,7 +37,7 @@ class Renderer{
     pixels.forEach(pixel=>{
       let color = pixel.color.value();
       Renderer.ctx.fillStyle = color;
-      Renderer.ctx.fillRect(pixel.position.x,pixel.position.y,1,1);
+      Renderer.ctx.fillRect(pixel.position2.x,pixel.position2.y,1,1);
     });
   }  
 }

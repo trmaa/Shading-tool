19911e73962094a040f1ea03848c6131e7d41efd
trmaa <trmaayt@gmail.com>
2023-12-17 21:21:24 +0100

    organitzated

---
+++ main.js
@@ -0,0 +1,8 @@
+class Main{
+	static main(){
+		Renderer.main();
+		Renderer.repaint(Renderer.pixels);
+	}
+}
+
+Main.main();
--- render.js
+++ render.js
@@ -1,10 +1,29 @@
+class Pixel{
+  color = new color(0,0,0);
+  position = new vec2(0,0);
+  constructor(color,position){
+    this.color = color;
+    this.position = position;
+  }
+}
+
 class Renderer{
   static cvs = document.querySelectorAll("canvas")[0];
   static ctx = Renderer.cvs.getContext("2d");
 
+  static pixels = [];
+
   static main(){
     Renderer.cvs.width = 500;
     Renderer.cvs.height = 500;
+
+    for(let x = 0;x < Renderer.cvs.width;x++){
+      for(let y = 0;y < Renderer.cvs.height;y++){
+        let id = y*Renderer.cvs.width-Renderer.cvs.width+x;
+        let maxid = Renderer.cvs.height*Renderer.cvs.width;
+        Renderer.pixels[id] = new Pixel(new color(id*255/maxid,0,0),new vec2(x,y));
+      }
+    }
   }
   
   static repaint(pixels){
@@ -15,24 +34,3 @@ class Renderer{
     });
   }  
 }
-Renderer.main();
-
-class Pixel{
-  color = new color(0,0,0);
-  position = new vec2(0,0);
-  constructor(color,position){
-    this.color = color;
-    this.position = position;
-  }
-}
-
-let pixels = [];
-for(let x = 0;x < Renderer.cvs.width;x++){
-  for(let y = 0;y < Renderer.cvs.height;y++){
-    let id = y*Renderer.cvs.width-Renderer.cvs.width+x;
-    let maxid = Renderer.cvs.height*Renderer.cvs.width;
-    pixels[id] = new Pixel(new color(id*255/maxid,0,0),new vec2(x,y));
-  }
-}
-
-Renderer.repaint(pixels);

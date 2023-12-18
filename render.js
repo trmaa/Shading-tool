2a12537933390cf343e94c3edf55dea97a75c5b9
trmaa <trmaayt@gmail.com>
2023-12-18 20:42:05 +0100

    fov

--- render.js
+++ render.js
@@ -12,7 +12,8 @@ class Renderer{
   static cvs = document.querySelectorAll("canvas")[0];
   static ctx = Renderer.cvs.getContext("2d");
 
-  static fov = 25;
+  static fov = 50;
+  static pov = {position:new vec3(0,0,0),angle:new vec2(0,0)};
 
   static pixels = [];
 
@@ -26,9 +27,11 @@ class Renderer{
         let maxid = Renderer.cvs.height*Renderer.cvs.width;
         Renderer.pixels[id] = new Pixel(new color(255,255-id*255/maxid,id*255/maxid),new vec2(x,y));
 
-        let w = x-Math.floor(Renderer.cvs.width*0.5);
-        let j = y-Math.floor(Renderer.cvs.height*0.5);
-        Renderer.pixels[id].position3 = new vec3(w,j,Renderer.fov);
+        //es un pov cuadrado porque las magnitudes solo son afectadas por un angulo. si fueran afectadas por las dos: seria redondo.
+        let w = Math.floor(Math.cos(Renderer.pov.angle.y)*Renderer.fov);
+        let j = Math.floor(Math.sin(Renderer.pov.angle.x)*Renderer.fov);
+        let k = Math.floor(Math.sin(Renderer.pov.angle.y)*Renderer.fov);
+        Renderer.pixels[id].position3 = new vec3(w,j,k);
       }
     }
   }

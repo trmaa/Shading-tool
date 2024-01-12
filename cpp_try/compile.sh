1ffac1f0281ebed69ce74fd3de4bc6ece4c37473
trmaa <trmaayt@gmail.com>
2024-01-12 12:59:54 +0100

    .exe

--- cpp_try/compile.sh
+++ cpp_try/compile.sh
@@ -1,1 +1,3 @@
-g++ -o mi_programa main.cpp -lGLEW -lglfw -lGL -I/usr/include -L/usr/lib/x86_64-linux-gnu -lGLEW -lglfw -lGL
+g++ -o render.exe main.cpp -lGLEW -lglfw -lGL -I/usr/include -L/usr/lib/x86_64-linux-gnu -lGLEW -lglfw -lGL
+
+./render.exe
